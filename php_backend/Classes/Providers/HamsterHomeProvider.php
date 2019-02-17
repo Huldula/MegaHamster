ae8c49c281897f9260151c6e5610af771a8ecfb4
Alexander Mittemueller <alexander.mittermueller@gmail.com>
2019-02-17 20:54:39 +0100

    read hamster homes from database, not tested yet

--- php_backend/Classes/Providers/HamsterHomeProvider.php
+++ php_backend/Classes/Providers/HamsterHomeProvider.php
@@ -2,9 +2,26 @@
 
 namespace HTL3R\MegaHamster\Providers;
 
+use \Doctrine\DBAL\Configuration;
+
 class HamsterHomeProvider {
 
+
     public static function loadAllHamsterHomes(): array {
-        
+        $connectionParams = array(
+            'dbname' => 'doctrineExample',
+            'user' => 'user',
+            'password' => 'password',
+            'host' => 'localhost',
+            'driver' => 'pdo_mysql',
+        );
+        $connection = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, new Configuration());
+
+        $queryBuilder = $connection->createQueryBuilder();
+        $queryBuilder
+            ->select('name, general_info, image_location')
+            ->from('homes');
+        $stmt = $queryBuilder->execute();
+        return $stmt->fetchAll();
     }
 }
